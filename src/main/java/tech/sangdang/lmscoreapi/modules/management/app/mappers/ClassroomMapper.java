ab73cfa83a1f15054e9fcde8b35f04ee3b8c016b
sang-dang-19 <danganhsang09@gmail.com>
2026-09-26 23:24:48 +0700

    Fix mapper

--- src/main/java/tech/sangdang/lmscoreapi/modules/management/app/mappers/ClassroomMapper.java
+++ src/main/java/tech/sangdang/lmscoreapi/modules/management/app/mappers/ClassroomMapper.java
@@ -4,17 +4,15 @@ import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
 import java.time.ZoneOffset;
 import org.mapstruct.Mapper;
-import tech.sangdang.lmscoreapi.common.querying.BaseQuery;
+import tech.sangdang.lmscoreapi.common.querying.QueryMapper;
 import tech.sangdang.lmscoreapi.generated.model.ClassroomFilter;
 import tech.sangdang.lmscoreapi.generated.model.ClassroomResponse;
 import tech.sangdang.lmscoreapi.modules.management.dom.Classroom;
 
 @Mapper(componentModel = "spring")
-public interface ClassroomMapper {
+public interface ClassroomMapper extends QueryMapper<ClassroomFilter> {
 
   ClassroomResponse toResponse(Classroom classroom);
-
-  BaseQuery toBaseQuery(ClassroomFilter apiFilter);
 
   default OffsetDateTime map(LocalDateTime value) {
     return value == null ? null : value.atOffset(ZoneOffset.UTC);
